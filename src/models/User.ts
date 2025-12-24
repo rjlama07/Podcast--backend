d9e646a988c4d0108d6f9b9f0ee3f093b8d3cbbc
Rj lama <riteshlama5@gmail.com>
2025-12-24 18:48:25 +1100

    add: user model and schema

---
+++ src/models/User.ts
@@ -0,0 +1,60 @@
+import { ObjectId, Schema } from "mongoose";
+
+interface User {
+  name: string;
+  email: string;
+  password: string;
+  verified: boolean;
+  avatar?: { url: string; publicId: string };
+  tokens: string[];
+  favourites: ObjectId[];
+  followers: ObjectId[];
+  followings: ObjectId[];
+}
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  avatar: {
+    type: Object,
+    url: String,
+    publicId: String,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  token: [String],
+  favourites: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Audio",
+    },
+  ],
+  followers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  followings: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
