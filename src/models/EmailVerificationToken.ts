3c196908972bd4d65c0fb5513a3e0fd7fb2e1c2b
Rj lama <riteshlama5@gmail.com>
2025-12-25 19:43:04 +1100

    feat: hashing passwords

--- src/models/EmailVerificationToken.ts
+++ src/models/EmailVerificationToken.ts
@@ -1,4 +1,5 @@
 import { ObjectId, Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
 
 interface EmailVerificationTokenDocument {
   owner: ObjectId;
@@ -20,9 +21,22 @@ const emailVerificaationTokenScheme =
     createdAt: {
       type: Date,
       expires: 3600,
-      default: Date.now(),
+      default: Date.now,
     },
   });
+
+emailVerificaationTokenScheme.pre("save", async function () {
+  if (this.isModified("token")) {
+    this.token = await bcrypt.hash(this.token, 10);
+  }
+});
+
+emailVerificaationTokenScheme.methods.compareToken = async function (
+  token: string
+) {
+  const result = await bcrypt.compare(token, this.token);
+  return result;
+};
 
 export default model<EmailVerificationTokenDocument>(
   "EmailVerificationToken",
